64924ec9592f1d3f2dee061f59d06b91b6f2868c
Davo <davidakinfenwa2@gmail.com>
2022-10-04 22:48:12 +0100

    old ios version fixes

--- js/main.js
+++ js/main.js
@@ -40,17 +40,20 @@
      }
  });
 
- document.querySelectorAll(".profile-container,.view-panel,.side-container,.col").forEach((element) => {
-     let observer = new IntersectionObserver((entries, observer)=>{
-         entries.forEach((entry)=>{
-             if(entry.isIntersecting){
-                 entry.target.classList.add("animate");
-                 observer.unobserve(entry.target);
-             }
-         });
-     });
-     observer.observe(element);
- });
+ if (typeof(IntersectionObserver) !== "undefined") {
+    document.querySelectorAll(".profile-container,.view-panel,.side-container,.col").forEach((element) => {
+        let observer = new IntersectionObserver((entries, observer)=>{
+            entries.forEach((entry)=>{
+                if(entry.isIntersecting){
+                    entry.target.classList.add("animate");
+                    observer.unobserve(entry.target);
+                }
+            });
+        });
+        observer.observe(element);
+    });
+    }
+
 
  //Email Js initialization
 (function () {
