02db81caf65b328b314c7d1d0275ce6038ed5f57
Davo <davidakinfenwa@gmail.com>
2023-02-20 11:13:36 +0100

    animation fix

--- js/display.js
+++ js/display.js
@@ -1,74 +1,83 @@
 let pause = false;
-        let all_indicators = document.querySelectorAll(".indicators > div");
-        let all_items = document.querySelectorAll(".item");
-        let timer;
-        let current_count = 0;
-        //Animates top indicator
+let all_indicators = document.querySelectorAll(".indicators > div");
+let all_items = document.querySelectorAll(".item");
+let timer;
+let current_count = 0;
+let anim_name = "backInLeft";
+//Animates top indicator
 
-        function indicator_control(elem, percent) {
+function indicator_control(elem, percent) {
 
-            let animatingElem = document.createElement("span");
-            //remove any previous element inside 
-            elem.innerHTML = "";
-            elem.appendChild(animatingElem);
-            animatingElem.style.width = `${percent}%`;
-        }
+    let animatingElem = document.createElement("span");
+    //remove any previous element inside 
+    elem.innerHTML = "";
+    elem.appendChild(animatingElem);
+    animatingElem.style.width = `${percent}%`;
+}
 
-        function toggleDisplay(index) {
-            all_items.forEach(function (elem) {
-                elem.classList.remove("active");
-            })
-            all_items[index].classList.add("active");
-            all_items[index].classList.add("rollIn");
+function toggleDisplay(index) {
+    all_items.forEach(function (elem) {
+        elem.classList.remove("active");
+    })
+    
+    //Remove all previous animation and add new one
+    all_items[index].classList.add("active");
+    all_items[index].classList.remove("backInLeft");
+    all_items[index].classList.remove("slideInRight");
+    all_items[index].classList.remove("slideInLeft");
+    all_items[index].classList.add(anim_name);
 
-        }
-        function switchDisplay(count = 0) {
-            let percent = 0;
-            timer = setInterval(function () {
-                console.log(count);
-                indicator_control(all_indicators[count], percent);
-                if (percent == 100) {
-                    percent = 0;
-                    count++;
-                    if(count >= all_items.length){count=0;}
-                    current_count = count;
-                    toggleDisplay(count);
-                }
-                percent++;
-            }, 100);
+}
+function switchDisplay(count = 0) {
+    let percent = 0;
 
+    timer = setInterval(function () {
+        indicator_control(all_indicators[count], percent);
+        if (percent == 100) {
+            anim_name = "backInLeft";
             percent = 0;
-
-            if(count >= all_items.length || count < 0){count=0;}
+            count++;
+            if (count >= all_items.length) { count = 0; }
             current_count = count;
-
             toggleDisplay(count);
-            return timer;
         }
+        percent++;
+    }, 100);
 
-        switchDisplay();
+    percent = 0;
 
-        all_items.forEach(function (elem) {
-            elem.onclick = function () {
-                pause = !pause;
-                clearInterval(timer);
-                if (pause == false) {
-                    timer = switchDisplay(current_count);
-                }
-            }
-        });
+    if (count >= all_items.length || count < 0) { count = 0; }
+    current_count = count;
 
-        //next and previous event listeners
-        document.querySelector(".left-side").onclick = function (params) {
-            clearInterval(timer);
-            indicator_control(all_indicators[current_count], 100);
-            timer = switchDisplay(current_count - 1);
+    toggleDisplay(count);
+    return timer;
+}
 
+switchDisplay();
+
+all_items.forEach(function (elem) {
+    elem.onclick = function () {
+        pause = !pause;
+        clearInterval(timer);
+        if (pause == false) {
+            timer = switchDisplay(current_count);
         }
+    }
+});
 
-        document.querySelector(".right-side").onclick = function (params) {
-            clearInterval(timer);
-            indicator_control(all_indicators[current_count], 100);
-            timer = switchDisplay(current_count + 1);
-        }
+//next and previous event listeners
+document.querySelector(".left-side").onclick = function (params) {
+    anim_name = "slideInRight";
+    clearInterval(timer);
+    indicator_control(all_indicators[current_count], 100);
+    timer = switchDisplay(current_count - 1);
+
+}
+
+document.querySelector(".right-side").onclick = function (params) {
+    anim_name = "slideInLeft";
+    clearInterval(timer);
+    indicator_control(all_indicators[current_count], 100);
+    timer = switchDisplay(current_count + 1);
+}
 
